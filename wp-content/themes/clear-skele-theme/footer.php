9d43a79a5e7a9689629395c02eebd3d5f86cf74a
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-02-28 21:22:15 +0000

    Updated gitignore to ignore WP install files, added code to the theme footer,

---
+++ wp-content/themes/clear-skele-theme/footer.php
@@ -0,0 +1,19 @@
+        </main>
+        <footer>
+            <div class="footerMain">
+                <div class="container">
+                    <div class="row"></div>
+                </div>
+            </div>
+            <div class="footerBottom">
+                <div class="container">
+                    <div class="row"></div>
+                </div>
+            </div>
+        </footer>
+        <?php wp_footer();?>
+        <script>
+            new WOW().init();
+        </script>
+    </body>
+</html>
