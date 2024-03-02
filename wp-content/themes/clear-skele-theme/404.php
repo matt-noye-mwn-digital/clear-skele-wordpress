04c8ede76b5f2db35918996b69c696142e90e660
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-03-02 00:24:22 +0000

    Setup 404 page

---
+++ wp-content/themes/clear-skele-theme/404.php
@@ -0,0 +1,17 @@
+<?php 
+    $fields = get_fields();
+    get_header();
+?>
+
+<section class="notFoundPageMain">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Unfortunately we couldn't find what you are looking for.</h1>
+                <p>Please go back and try again.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
