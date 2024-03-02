60d927c27c16d5dec9090a34d2e3b557438faefe
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-03-02 00:23:28 +0000

    Added copyright and default site by to footer.

--- wp-content/themes/clear-skele-theme/footer.php
+++ wp-content/themes/clear-skele-theme/footer.php
@@ -7,7 +7,16 @@
             </div>
             <div class="footerBottom">
                 <div class="container">
-                    <div class="row"></div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <p>&copy; Copyright <?=bloginfo();?> <?= date('Y');?> | All Rights Reserved</p>
+                        </div>
+                        <div class="col-lg-6 text-lg-end">
+                            <p>
+                                Site by <a href='' target='_blank' rel='nofollow'>Company Name Here</a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
