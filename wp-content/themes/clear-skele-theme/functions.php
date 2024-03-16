841eb2827f3d4606ca683aeec618d680908e8d15
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-03-16 21:59:23 +0000

    Updates to the theme function files, added in default options pages, added in some ACF code.

--- wp-content/themes/clear-skele-theme/functions.php
+++ wp-content/themes/clear-skele-theme/functions.php
@@ -4,6 +4,8 @@
     require get_template_directory().'/inc/widget_areas.php';
     require get_template_directory().'/inc/createRequiredPages.php';
     require get_template_directory().'/inc/customAcfOptionsPages.php';
+    require get_template_directory().'/inc/customAcfRequiredFields.php';
     require get_template_directory().'/inc/customizer.php';
     require get_template_directory().'/inc/class-wp-bootstrap-navwalker.php';
     require get_template_directory().'/inc/plugin_includes.php';
+    
--- wp-content/themes/clear-skele-theme/partials/main_menu.php
+++ wp-content/themes/clear-skele-theme/partials/main_menu.php
@@ -2,7 +2,11 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg">
             <a href="" class="navbar-brand">
-                <?=bloginfo();?>
+                <?php if(get_field('main_logo', 'option')): ?>
+                    <img src="<?=get_field('main_logo', 'option');?>" alt="" class="navbar-brand-image">
+                <?php else: ?>
+                    <img src="<?=get_template_directory_uri();?>/assets/images/image-coming-soon.jpg" alt="" class="navbar-brand-image-temp" style=' height: 75px; margin:0; width: auto'>
+                <?php endif; ?>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
