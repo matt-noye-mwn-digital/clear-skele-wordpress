5e7a05a75657458464b3db70311bd723c5828502
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-04-24 20:18:50 +0100

    Updates to files and also fix to the header mobile nav not working as expected

--- wp-content/themes/clear-skele-theme/partials/main_menu.php
+++ wp-content/themes/clear-skele-theme/partials/main_menu.php
@@ -8,7 +8,7 @@
                     <img src="<?=get_template_directory_uri();?>/assets/images/image-coming-soon.jpg" alt="" class="navbar-brand-image-temp" style=' height: 75px; margin:0; width: auto'>
                 <?php endif; ?>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
             <?php 
