35f0fe63889a834583fe041959064420890f9ac8
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-08-15 13:40:37 +0100

    Updated GULP file so that when any file in the scss folder is saved it runs the script

--- wp-content/themes/clear-skele-theme/gulpfile.mjs
+++ wp-content/themes/clear-skele-theme/gulpfile.mjs
@@ -3,31 +3,32 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import autoprefixer from 'gulp-autoprefixer';
-import rename from 'gulp-rename';  // Import the rename plugin
+import rename from 'gulp-rename';
 
 const sass = gulpSass(dartSass);
 
 const paths = {
   scss: {
-    src: 'assets/scss/main.scss',
+    src: 'assets/scss/**/*.scss',  // Watch all SCSS files in the folder
+    main: 'assets/scss/main.scss', // Path to main.scss
     dest: 'assets/css',
   },
 };
 
 function styles() {
   return gulp
-    .src(paths.scss.src)
+    .src(paths.scss.main)  // Always compile main.scss
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       cascade: false,
     }))
     .pipe(cleanCSS())
-    .pipe(rename('main.min.css'))  // Rename the output file to main.min.css
+    .pipe(rename('main.min.css'))
     .pipe(gulp.dest(paths.scss.dest));
 }
 
 function watch() {
-  gulp.watch(paths.scss.src, styles);
+  gulp.watch(paths.scss.src, styles);  // Watch all SCSS files
 }
 
 const build = gulp.series(styles, watch);
